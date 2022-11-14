d1bfcb2f5ca3be41f4c85df71d14e3ecb45f5972
Julia Portseva <104313259+wrongju@users.noreply.github.com>
2022-11-14 13:47:26 +0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,30 @@
+let shareBlock = document.querySelector('.block-share');
+let share = document.querySelector('.share-bc');
+let share2 = document.querySelector('.share-bc2');
+
+
+share.addEventListener('click', event => {
+	if (share.classList.contains('selected')) {
+		shareBlock.style.display = 'none';
+		share.classList.remove('selected');
+	} else {
+		shareBlock.style.display = 'block';
+		share.classList.add('selected');
+	}
+})
+
+share2.addEventListener('click', event => {
+	if (share2.classList.contains('selected')) {
+		shareBlock.style.display = 'none';
+		share2.classList.remove('selected');
+	} else {
+		shareBlock.style.display = 'block';
+		share2.classList.add('selected');
+	}
+})
+
+document.addEventListener('mouseup', event => {
+	if (!shareBlock.contains(event.target)) {
+		shareBlock.style.display = 'none';
+	}
+})
